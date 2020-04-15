909f9125ed9569ceb1073353022c260bd9c807be
remo <remo555@hotmail.nl>
2020-04-15 20:36:55 +0200

    CMS toevoeging

---
+++ Public/CMS/Index.php
@@ -0,0 +1,13 @@
+<link rel="stylesheet" type="text/css" href="Stylesheet/Stylesheet.css">
+    <div id="container">
+        <div id="Rightbar"></div>
+        <div id="Leftbar"></div>
+        <div id="topbar">
+            <div id="Text-align">
+                <a id="hyperlink-Home" href="Index.php">Home</a>
+                <a id="hyperlink-Menu" href="Index.php">Menu</a>
+                <a id="hyperlink-Inloggen" href="Login.php">Login</a>
+            </div>
+        </div>
+        <div id="Text-middle"><p>Quisque ac ultricies neque, vel ornare ligula. Pellentesque pulvinar leo eget mauris blandit dapibus. Nam sollicitudin congue elit, a porta sem. Nulla sit amet congue arcu. Praesent vestibulum rhoncus risus ac commodo. Quisque id sem justo. In ac eros et orci elementum suscipit. Suspendisse vitae elit quis lorem maximus tempor. Suspendisse potenti. Suspendisse potenti. Morbi dapibus, nulla vitae finibus auctor, ex ipsum venenatis sapien, vel facilisis sapien ex aliquet ligula. Sed nec felis sit amet orci tincidunt venenatis ac a mi. Suspendisse sagittis magna lacinia tortor bibendum posuere. In efficitur, lectus quis condimentum lobortis, erat ex facilisis orci, vel volutpat mauris mi quis augue. Sed posuere eget nibh consectetur ultrices.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu dignissim ipsum. Donec lobortis tellus eget neque commodo ornare. Donec pharetra, orci eget tempus pretium, eros sem vestibulum nisi, non suscipit mi mi ac metus. Morbi mauris justo, facilisis at condimentum quis, vestibulum in libero. Sed ullamcorper massa fermentum, sagittis ligula in, vehicula turpis. Mauris tincidunt nibh quis elit faucibus vehicula. Cras porttitor, libero non finibus tristique, tortor eros varius nulla, ut consequat tellus nibh a quam. Aliquam erat volutpat. Aliquam nec ipsum in nibh tincidunt porttitor. Pellentesque id tortor libero.</p></div>
+</div>
